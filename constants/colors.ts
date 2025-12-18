d24b420802607294b2c29d8caa5b382530257632
Heston-M <hestonmountain@gmail.com>
2025-12-17 18:04:11 -0600

    Added color scheme with hook to access

---
+++ constants/colors.ts
@@ -0,0 +1,28 @@
+
+export const colors = {
+  light: {
+    background: "white",
+    text: "black",
+  },
+  dark: {
+    background: "black",
+    text: "white",
+  },
+}
+
+export const tileColors = {
+  light: {
+    default: "white",
+    hover: "lightgray",
+    border: "black",
+    selected: "blue",
+    territory: "green",
+  },
+  dark: {
+    default: "black",
+    hover: "darkgray",
+    border: "white",
+    selected: "blue",
+    territory: "green",
+  },
+}
