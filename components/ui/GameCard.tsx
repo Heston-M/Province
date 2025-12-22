b9158984e42c5f206ebce0e28cefeceff4f0f315
Heston-M <hestonmountain@gmail.com>
2025-12-22 13:42:34 -0600

    Added GameCard to display a GameConfig

---
+++ components/ui/GameCard.tsx
@@ -0,0 +1,52 @@
+import { useThemeContext } from "@/contexts/ThemeContext";
+import { GameConfig } from "@/types/gameConfig";
+import { formatTime } from "@/utils/timeUtils";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+interface GameCardProps {
+  game: GameConfig;
+  isSelected: boolean;
+  onGamePressed: (game: GameConfig) => void;
+}
+
+export default function GameCard({ game, isSelected, onGamePressed }: GameCardProps) {
+  const { getThemeColor } = useThemeContext();
+  const backgroundColor = getThemeColor("background");
+  const accentColor = getThemeColor("accent");
+  const textColor = getThemeColor("text");
+  const borderColor = getThemeColor("border");
+
+  const timeLimit = game.timeLimit === -1 ? "None" : formatTime(game.timeLimit);
+
+  return (
+    <Pressable 
+      style={[styles.container, { backgroundColor: backgroundColor, borderColor: isSelected ? accentColor : borderColor }]}
+      onPress={() => onGamePressed(game)}
+    >
+      <Text style={[styles.title, { color: textColor }]}>{game.name}</Text>
+      {game.description ? <Text style={[styles.description, { color: textColor }]}>{game.description}</Text> : null}
+      <Text style={[styles.description, { color: textColor }]}>Board Size: {game.boardSize[0]}x{game.boardSize[1]}</Text>
+      <Text style={[styles.description, { color: textColor }]}>{game.fogOfWar ? "Fog of War" : "No Fog of War"}</Text>
+      <Text style={[styles.description, { color: textColor }]}>{game.timeLimit === -1 ? "No Time Limit" : `Time Limit: ${timeLimit}`}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    gap: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 12,
+  },
+});
