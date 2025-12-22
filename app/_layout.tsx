cd78ad3d389b3ca21d180015e54daba092f4e844
Heston-M <hestonmountain@gmail.com>
2025-12-22 16:58:04 -0600

    Created UserContext to manage custom game memory

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { GameplayProvider } from "@/contexts/GameplayContext";
 import MenuContextProvider from "@/contexts/MenuContext";
 import ThemeContextProvider from "@/contexts/ThemeContext";
+import UserContextProvider from "@/contexts/UserContext";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -8,17 +9,19 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <ThemeContextProvider>
-        <GameplayProvider>
-          <MenuContextProvider>
-            <Stack 
-            screenOptions={{
-              headerShown: false,
-            }}
-            >
-              <Stack.Screen name="index" options={{ title: "Province" }} />
-            </Stack>
-          </MenuContextProvider>
-        </GameplayProvider>
+        <UserContextProvider>
+          <GameplayProvider>
+            <MenuContextProvider>
+              <Stack 
+              screenOptions={{
+                headerShown: false,
+              }}
+              >
+                <Stack.Screen name="index" options={{ title: "Province" }} />
+              </Stack>
+            </MenuContextProvider>
+          </GameplayProvider>
+        </UserContextProvider>
       </ThemeContextProvider>
     </SafeAreaProvider>
   );
